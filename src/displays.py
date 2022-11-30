65fa2d53b86dde9f7ce0d851b667a54c206aa741
Toonlink3000 <danielcoats@seznam.cz>
2022-11-30 20:57:36 +0100

    Started Code restructure

--- src/displays.py
+++ src/displays.py
@@ -1,5 +1,6 @@
 from customtkinter import *
 from tkinter import *
+import widgets
 
 
 class DisplayManager():
@@ -9,7 +10,7 @@ class DisplayManager():
 
 	def jump_to_display(self, display:int, **displayargs):
 		self.clear_window()
-		DISPLAYS[self.current_display](display, displayargs)
+		DISPLAYS[self.current_display](self.window, displayargs)
 
 
 	def clear_window(self):
@@ -19,7 +20,21 @@ class DisplayManager():
 
 class StartScreen():
 	def __init__(self, window, displayargs:dict):
-		pass
+		window.control_buttons = widgets.ControlButtons(window, self.open_quiz)
+		window.control_buttons.grid(row=2, column=0, pady=50)
+		window.title_text = CTkLabel(text="FreeQuiz", text_font=("default", -70))
+		window.title_text.grid(row=0, column=0, pady=50)
+
+		window.grid_columnconfigure(0, weight=1)
+		window.grid_rowconfigure(1, weight=1)
+
+	def open_quiz(self):
+		file_name = askopenfilename(filetypes=(("OpenQuiz quiz file", "*.qwz"), ("JSON files", "*.json"), ("All files", "*")))
+		file = open(file_name, "r")
+		data = json.loads(file.read())
+		file.close()
+		self.quiz = Quiz(self, data)
+		self.quiz.start_quiz()
 
 class QuizWelcome():
 	def __init__(self, window, displayargs:dict):
--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 from customtkinter import *
 from tkinter.filedialog import *
-import widgets
-from displays import DisplayManager
+import displays
 import sys
 import json
 
@@ -9,25 +8,8 @@ class App(CTk):
 	def __init__(self):
 		super().__init__()
 		self.title("OpenQuiz")
-		self.display_manager = 
-		self.show_start_screen()
-
-	def show_start_screen(self):
-		self.control_buttons = widgets.ControlButtons(self, self.open_quiz)
-		self.control_buttons.grid(row=2, column=0, pady=50)
-		self.title_text = CTkLabel(text="OpenQuiz", text_font=("default", -70))
-		self.title_text.grid(row=0, column=0, pady=50)
-
-		self.grid_columnconfigure(0, weight=1)
-		self.grid_rowconfigure(1, weight=1)
-
-	def open_quiz(self):
-		file_name = askopenfilename(filetypes=(("OpenQuiz quiz file", "*.qwz"), ("JSON files", "*.json"), ("All files", "*")))
-		file = open(file_name, "r")
-		data = json.loads(file.read())
-		file.close()
-		self.quiz = Quiz(self, data)
-		self.quiz.start_quiz()
+		self.display_manager = displays.DisplayManager(self)
+		self.display_manager.jump_to_display(0)
 
 class Quiz():
 	def __init__(self, win, quiz_data, display_manager):
@@ -36,22 +18,17 @@ class Quiz():
 		self.display_manager = display_manager
 
 	def display_welcome(self):
+		pass
 
-	def start_quiz(self)
+	def start_quiz(self):
 		self.clear_window()
 		self.question_display = widgets.QuestionDisplay(self, quiz_data["name"], "hello world")
 		self.question_display.grid(row=0, column=0, padx=20, pady=20, sticky="ew")
 		self.grid_columnconfigure(0, weight=1)
 
-
-
 def main():
-	try:
-		app = App()
-		app.mainloop()
-
-	except Exception as exc:
-		pass
+	app = App()
+	app.mainloop()
 
 if __name__ == '__main__':
 	main()
